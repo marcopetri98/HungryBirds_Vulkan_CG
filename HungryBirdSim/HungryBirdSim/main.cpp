886910651a7b84710067448022d33f61aebd2db3
EtionPinari <etion.pinari@mail.polimi.it>
2022-06-19 10:58:06 +0200

    Update main.cpp

--- HungryBirdSim/HungryBirdSim/main.cpp
+++ HungryBirdSim/HungryBirdSim/main.cpp
@@ -15,8 +15,6 @@ struct GlobalUniformBufferObject {
 // The uniform buffer object used in this example
 struct UniformBufferObject {
 	alignas(16) glm::mat4 model;
-	alignas(16) glm::mat4 view;
-	alignas(16) glm::mat4 proj;
 };
 
 // MAIN ! 
